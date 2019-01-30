0a34bcd6c04ab18165b4831c260cebfb0be90549
krishtal <krishtalforkato@gmail.com>
2019-01-30 15:25:28 +0200

    Types

---
+++ src/serialization/ContainerType.java
@@ -0,0 +1,12 @@
+package serialization;
+
+public class ContainerType {
+
+	public static byte UNKNOWN 	= 0;
+	public static byte FIELD 	= 1;
+	public static byte ARRAY 	= 2;
+	public static byte STRING	= 3;
+	public static byte OBJECT 	= 4;
+	public static byte DATABASE = 5;
+	
+}
